cf45542f4e1ff9e0c0e51e00bbda27e79350f274
christiankelechi <kezechristian@gmail.com>
2024-12-18 01:31:42 +0100

    current update on ui

--- imagelabelbox/settings.py
+++ imagelabelbox/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "imagelabelbox.urls"
@@ -129,6 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL="media/"
+STATIC_FILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- imagelabelbox/urls.py
+++ imagelabelbox/urls.py
@@ -30,6 +30,4 @@ urlpatterns = [
     path('save_annotations/<int:image_id>/', views.save_annotations, name='save_annotations'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
